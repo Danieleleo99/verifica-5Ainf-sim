e0f0ae6e2a7386c88687174e12e2980ff5bd0fe4
danilo <ddanilo731@gmail.com>
2018-11-10 10:36:43 +0100

    consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
@@ -12,15 +12,26 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(600,400);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
+		StringBuilder sb = new StringBuilder();
+		
 		
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<html>");
+		sb.append("<h1>Formula 1 passion</h1>");
+		sb.append("<div style='display:table-row'>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<div style='display:table'>");
+		
 		
 		// TODO Convertire lo StringBuilder in String
 		
+		
 		// TODO Assegnare correttamente la stringa create a JLabel
+		
 		super.add(new JLabel(__TODO__));
 		super.setVisible(true);
 		 
